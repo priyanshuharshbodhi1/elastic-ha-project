59ef69da96e93a934d06725a5785eb587f1783bc
ahmadnurfadilah <ahmadnurfadilah22@gmail.com>
2024-08-22 17:01:38 +0700

    add data to dashboard

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -1,5 +1,130 @@
+"use client";
+
+import { Card } from "@/components/ui/card";
+import { useTeam } from "@/lib/store";
+import { ArrowRight, Frown, Meh, MessageSquare, MessageSquareDashed, MessageSquarePlus, Smile, SmilePlus, Star } from "lucide-react";
+import { useEffect, useState } from "react";
+import TopKeywords from "./top-keywords";
+
 export default function Dashboard() {
+  const team = useTeam((state) => state.team);
+  const [stats, setStats] = useState<any>();
+  const [topKeywords, setTopKeywords] = useState<any[]>([]);
+
+  useEffect(() => {
+    const getStats = async () => {
+      fetch(`/api/team/${team.id}/stats/dashboard`)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.success) {
+            setStats(data.data);
+          }
+        })
+        .catch((err) => console.log(err));
+    };
+
+    const getTopKeywords = async () => {
+      fetch(`/api/team/${team.id}/stats/top-keywords`)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.success) {
+            setTopKeywords(data.data);
+          }
+        })
+        .catch((err) => console.log(err));
+    };
+
+    if (team) {
+      getStats();
+      getTopKeywords();
+    }
+  }, [team]);
+
   return (
-    <h1>Dashboard</h1>
+    <>
+      <div className="flex items-center justify-between mb-4">
+        <h1 className="font-bold text-xl">Dashboard</h1>
+      </div>
+
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-6 mb-6">
+        <div className="w-full bg-white rounded border p-4 shadow-sm">
+          <div className="flex items-center justify-between mb-2">
+            <div className="flex items-center gap-2">
+              <MessageSquarePlus className="w-5 h-5" />
+              <h4 className="font-bold text-lg">{stats?.total}</h4>
+            </div>
+            <ArrowRight className="w-4 h-4" />
+          </div>
+          <p className="text-sm">Feedback Added</p>
+        </div>
+        <div className="w-full bg-white rounded border p-4 shadow-sm">
+          <div className="flex items-center justify-between mb-2">
+            <div className="flex items-center gap-2">
+              <MessageSquareDashed className="w-5 h-5" />
+              <h4 className="font-bold text-lg">{stats?.open}</h4>
+            </div>
+            <ArrowRight className="w-4 h-4" />
+          </div>
+          <p className="text-sm">Feedback Open</p>
+        </div>
+        <div className="w-full bg-white rounded border p-4 shadow-sm">
+          <div className="flex items-center justify-between mb-2">
+            <div className="flex items-center gap-2">
+              <MessageSquare className="w-5 h-5" />
+              <h4 className="font-bold text-lg">{stats?.resolved}</h4>
+            </div>
+            <ArrowRight className="w-4 h-4" />
+          </div>
+          <p className="text-sm">Feedback Resolved</p>
+        </div>
+        <div className="w-full bg-white rounded border p-4 shadow-sm">
+          <div className="flex items-center justify-between mb-2">
+            <div className="flex items-center gap-2">
+              <Star className="w-5 h-5" />
+              <h4 className="font-bold text-lg">{stats?.ratingAverage}</h4>
+            </div>
+            <ArrowRight className="w-4 h-4" />
+          </div>
+          <p className="text-sm">Rating Average</p>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div className="md:col-span-2">
+          <Card className="p-4">
+            <h3 className="font-bold mb-2">Top Keywords</h3>
+            <TopKeywords data={topKeywords} />
+          </Card>
+        </div>
+        <div>
+          <Card className="p-4">
+            <h3 className="font-bold mb-2">Sentiment</h3>
+            <div className="divide-y-2 font-medium">
+              <div className="flex items-center justify-between">
+                <div className="flex items-center gap-1 py-2 text-green-500">
+                  <Smile className="w-4 h-4" />
+                  <p>Positive</p>
+                </div>
+                <p className="font-bold font-mono">{stats?.sentiment?.find((o: any) => o.name === "positive")?.percentage}%</p>
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex items-center gap-1 py-2">
+                  <Meh className="w-4 h-4" />
+                  <p>Neutral</p>
+                </div>
+                <p className="font-bold font-mono">{stats?.sentiment?.find((o: any) => o.name === "neutral")?.percentage}%</p>
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex items-center gap-1 py-2 text-red-500">
+                  <Frown className="w-4 h-4" />
+                  <p>Negative</p>
+                </div>
+                <p className="font-bold font-mono">{stats?.sentiment?.find((o: any) => o.name === "negative")?.percentage}%</p>
+              </div>
+            </div>
+          </Card>
+        </div>
+      </div>
+    </>
   );
 }
--- app/(app)/select-team.tsx
+++ app/(app)/select-team.tsx
@@ -9,9 +9,8 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { useTeam, useUser } from "@/lib/store";
-import { CheckIcon, ChevronDown } from "lucide-react";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { ChevronDown } from "lucide-react";
+import { useEffect } from "react";
 import toast from "react-hot-toast";
 
 export default function SelectTeam({ session }: { session: any }) {
@@ -41,60 +40,6 @@ export default function SelectTeam({ session }: { session: any }) {
     }
   }, [user, activeTeam, setActiveTeam]);
 
-  // const router = useRouter();
-  // const [selectedMerchant, setSelectedMerchant] = useState();
-  // const [merchants, setMerchants] = useState([]);
-  // const activeTeam = useTeam((state) => state.team);
-  // const setActiveTeam = useTeam((state) => state.setTeam);
-
-  // useEffect(() => {
-  //   const getMerchants = async () => {
-  //     const { data, error } = await supabase.from("merchants").select("*").eq("user_id", session?.user?.id);
-  //     if (!error) {
-  //       if (data.length > 0) {
-  //         setMerchants(data);
-  //         setSelectedMerchant(data[0]);
-  //       } else {
-  //         router.push("/connect");
-  //       }
-  //     }
-  //   };
-
-  //   getMerchants();
-  // }, [supabase, session]);
-
-  // useEffect(() => {
-  //   if (selectedMerchant) {
-  //     fetch(`/api/customers/sync?token=${selectedMerchant.access_token}&merchant_id=${selectedMerchant.id}`);
-  //     setActiveMerchant(selectedMerchant);
-  //   }
-  // }, [selectedMerchant, setActiveMerchant]);
-
-  // const handleSelectedMerchant = async (e) => {
-  //   const selectedData = find(merchants, (o) => o.id === e);
-  //   setSelectedMerchant(selectedData);
-  //   await supabase
-  //     .from("merchants")
-  //     .update({
-  //       is_active: false,
-  //     })
-  //     .eq("user_id", session.user.id)
-  //     .select();
-
-  //   await supabase
-  //     .from("merchants")
-  //     .update({
-  //       is_active: true,
-  //     })
-  //     .eq("user_id", session.user.id)
-  //     .eq("mid", selectedData.mid)
-  //     .select();
-  // };
-
-  // if (!activeMerchant) {
-  //   return null;
-  // }
-
   return (
     <>
       <div className="p-4">
@@ -114,43 +59,6 @@ export default function SelectTeam({ session }: { session: any }) {
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
-
-      {/* <Listbox value={selectedMerchant?.id} onChange={handleSelectedMerchant}>
-        <div className="relative">
-          <Listbox.Button className="relative w-full cursor-pointer h-16 bg-gray-700 text-white font-bold py-2 pl-4 pr-10 text-left shadow-sm focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-            <span className="block truncate">{selectedMerchant?.name}</span>
-            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-4">
-              <ChevronDown />
-            </span>
-          </Listbox.Button>
-          <Transition as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
-            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-              {merchants &&
-                merchants.length > 0 &&
-                merchants.map((i, idx) => (
-                  <Listbox.Option
-                    key={idx}
-                    className={({ active }) =>
-                      `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? "bg-pink-100 text-pink-900" : "text-gray-900"}`
-                    }
-                    value={i.id}
-                  >
-                    {({ selected }) => (
-                      <>
-                        <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>{i.name}</span>
-                        {selected ? (
-                          <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-pink-600">
-                            <CheckIcon className="w-4 h-4" />
-                          </span>
-                        ) : null}
-                      </>
-                    )}
-                  </Listbox.Option>
-                ))}
-            </Listbox.Options>
-          </Transition>
-        </div>
-      </Listbox> */}
     </>
   );
 }
